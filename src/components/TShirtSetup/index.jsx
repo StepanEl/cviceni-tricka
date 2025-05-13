a9733535fe3172436d258942b0fa8aab60c05c5b
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-05-13 23:05:26 +0200

    Done

--- src/components/TShirtSetup/index.jsx
+++ src/components/TShirtSetup/index.jsx
@@ -1,10 +1,12 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({onTypeChange, onColorChange, onPrintChange}) => {
+
+
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={(event)=>{onTypeChange(event.target.value)}}>
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,7 +16,7 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={(event)=>{onColorChange(event.target.value)}}>
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
@@ -22,7 +24,7 @@ export const TShirtSetup = () => {
       </select>
     
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" onChange={(event)=>{onPrintChange(event.target.value)}}/>
     </form>
   );
 };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import { TShirt } from '../../components/TShirt';
 import { TShirtSetup } from '../../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+  const [type, setType] = useState("normalShortSleeve")
+  const [color, setColor] = useState("yellow")
+  const [print, setPrint] = useState("Ahoj světe")
+
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt
+            type={type}
+            color={color}
+            print={print}
+          />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onTypeChange={(hodnota) =>{ setType(hodnota); console.log(hodnota)}} 
+            onColorChange={(hodnota) =>{setColor(hodnota)}}
+            onPrintChange={(hodnota)=>{setPrint(hodnota)}}/>
         </div>
       </div>
     </div>
